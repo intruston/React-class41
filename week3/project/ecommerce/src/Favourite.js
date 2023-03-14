a02efcde20aaf1c11108be25ed0ce61b9db5c757
intruston <intrustonly@gmail.com>
2023-03-14 20:26:20 +0100

    Updated Favourite

--- week3/project/ecommerce/src/Favourite.js
+++ week3/project/ecommerce/src/Favourite.js
@@ -3,12 +3,12 @@ import heartRegular from "./heart-regular.svg";
 import heartSolid from "./heart-solid.svg";
 
 function Favourite({ productId, isFavourited, onClick }) {
-  const handleClick = () => {
+  const heartClick = () => {
     onClick(productId);
   };
 
   return (
-    <div className="product-image--favourite-container" onClick={handleClick}>
+    <div className="product-image--favourite-container" onClick={heartClick}>
       <img src={isFavourited ? heartSolid : heartRegular} alt="Favourite" />
     </div>
   );
